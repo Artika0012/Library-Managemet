5f1dd980bc9c14e5108f1fbde97fe68afba8047b
Artika0012 <83173759+Artika0012@users.noreply.github.com>
2022-08-17 23:10:31 +0530

    Add files via upload

---
+++ public/src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <h4 style={{ textAlign: "center", color: "wheat" }}>PageNotFound</h4>
+      <a href="/">Back To Home</a>
+    </div>
+  );
+};
+
+export default PageNotFound;
